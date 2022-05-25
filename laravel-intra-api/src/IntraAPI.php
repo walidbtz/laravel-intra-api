7045e43be79fc01e5bafa71e6e39c7f78242bdc2
walidbtz <98676215+walidbtz@users.noreply.github.com>
2022-05-25 16:37:04 +0100

    Add files via upload

---
+++ laravel-intra-api/src/IntraAPI.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Naviisml\IntraApi;
+
+use Naviisml\IntraApi\Api\ApiWrapper;
+
+class IntraAPI extends ApiWrapper
+{
+	/**
+	 * [__construct description]
+	 *
+	 * @return  [type]  [return description]
+	 */
+	public function __construct()
+	{
+		return $this;
+	}
+}
